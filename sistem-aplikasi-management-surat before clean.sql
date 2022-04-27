ab9f202f134e98f77ca7d8891d0d4e62b5791d53
aderahman007 <aderahman9908@gmail.com>
2022-04-27 13:35:32 +0700

    clean database

--- sistem-aplikasi-management-surat before clean.sql
+++ sistem-aplikasi-management-surat before clean.sql
@@ -34,9 +34,25 @@ CREATE TABLE `disposisi` (
   PRIMARY KEY (`id`),
   KEY `id_surat` (`id_surat`),
   CONSTRAINT `disposisi_ibfk_1` FOREIGN KEY (`id_surat`) REFERENCES `surat` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `disposisi` */
+
+insert  into `disposisi`(`id`,`id_surat`,`tanggal_disposisi`,`tujuan_disposisi`,`nama_instansi`,`jabatan`,`tipe_disposisi`,`disposisi`,`catatan`,`is_deleted`) values 
+(1,1,'2022-04-21 21:48:36','33','Indra Hermawan, S.H.','Seketaris',1,'Telah diterima','Sudah diterima\r\n',0),
+(2,2,'2022-04-25 17:08:13',NULL,'fsdfsdf','dfsdf',1,'fsdfsdfsdf','fsdfsdfsf',0),
+(3,1,'2022-04-25 17:09:21','33','gsdgdg','gdgf',1,'gdgdfg','dfgdgdfg',0),
+(4,1,'2022-04-14 00:00:00','33',NULL,NULL,2,NULL,NULL,0),
+(5,1,'2022-04-10 00:00:00','33',NULL,NULL,2,NULL,NULL,0),
+(6,7,'2022-04-12 00:00:00','54',NULL,NULL,3,'Cras ultricies ligula sed magna dictum porta.',NULL,0),
+(7,1,'2022-04-22 00:00:00','48',NULL,NULL,2,'gdfgdfg',NULL,0),
+(8,16,'2022-04-22 00:00:00','2',NULL,NULL,3,'Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.',NULL,0),
+(9,2,'2022-04-07 00:00:00','33',NULL,NULL,2,'Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.',NULL,0),
+(10,5,'2022-04-23 00:00:00','33',NULL,NULL,2,'Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.',NULL,0),
+(13,7,'2022-04-27 11:29:45','34',NULL,NULL,3,'Donec sollicitudin molestie malesuada.',NULL,0),
+(14,18,'2022-04-27 11:31:25','33',NULL,NULL,3,'Donec sollicitudin molestie malesuada.',NULL,0),
+(15,18,'2022-04-27 11:32:22','36',NULL,NULL,2,'dgdfgd',NULL,0),
+(16,18,'2022-04-27 11:34:00','34',NULL,NULL,3,'bcbcvbcvbb',NULL,0);
 
 /*Table structure for table `groups` */
 
@@ -82,7 +98,11 @@ insert  into `instansi`(`id`,`instansi`,`alamat`,`is_deleted`,`date_deleted`) va
 (8,'Ramayana Tanjung Karang','Jl. Kota Raja No.27, Gn. Sari, Engal, Kota Bandar Lampung, Lampung',0,NULL),
 (9,'PDAM Way Rilau','Jl. P. Emir Moh. Noer No.11a, Sumur Putri, Kec. Tlk. Betung Utara, Kota Bandar Lampung',0,NULL),
 (10,'PDAM Tirta Jasa unit waykandis','Jl. Pdam Raya I, Way Huwi, Kec. Jati Agung, Kabupaten Lampung Selatan',0,NULL),
-(11,'PDAM PRINGSEWU WAY SEKAMPUNG','Jl. A.Yani No.509, Sidoharjo, Kec. Pringsewu, Kabupaten Pringsewu, Lampung',0,NULL);
+(11,'PDAM PRINGSEWU WAY SEKAMPUNG','Jl. A.Yani No.509, Sidoharjo, Kec. Pringsewu, Kabupaten Pringsewu, Lampung',0,NULL),
+(12,'xcvxcv','vxcvxcv',0,NULL),
+(13,'xcvxcv5455','fdgdfgdg',0,NULL),
+(14,'sfsfsff35345','gfdfgdgfd',0,NULL),
+(15,'xcvxcv','bdbdfgdfg',0,NULL);
 
 /*Table structure for table `jabatan` */
 
@@ -108,7 +128,10 @@ insert  into `jabatan`(`id`,`jabatan`,`parent`,`is_deleted`,`date_deleted`) valu
 (37,'KaSubBag Perencanaan',36,0,NULL),
 (38,'KaSubBag Pembukuan dan Verifikasi',36,0,NULL),
 (39,'KasSubBag Perjalanan Dinas',36,0,NULL),
-(40,'KaSubBag Pembukuan dan Verifikasi',36,0,NULL);
+(40,'KaSubBag Pembukuan dan Verifikasi',36,0,NULL),
+(48,'dsfsfs45466',47,0,NULL),
+(54,'Kepala Humas',0,0,NULL),
+(55,'Bagian Nganggur',33,1,'2022-04-27 02:17:26');
 
 /*Table structure for table `login_attempts` */
 
@@ -159,9 +182,24 @@ CREATE TABLE `surat` (
   `is_deleted` tinyint(4) DEFAULT '0',
   `date_deleted` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `surat` */
+
+insert  into `surat`(`id`,`tipe_surat`,`nomor_surat`,`tanggal_surat`,`isi`,`tanggal_diterima`,`sifat_surat`,`kecepatan_surat`,`asal_surat`,`tujuan_surat`,`nomor_agenda`,`file`,`status_surat`,`penerima`,`is_deleted`,`date_deleted`) values 
+(1,'masuk','B-DL.01.01/184/2022','2022-01-25 00:00:00','Pemanggilan peserta diklat Fungsional Pengangkatan Arsiparis Tingkat Ahli Angkatan 1','2022-01-26 00:00:00','rahasia','segera',4,32,NULL,NULL,'selesai',1,0,NULL),
+(2,'masuk','B-AK.01.01/25/2022','2022-02-02 00:00:00','Laporan Hasil Pengawasan Kearsipan','2022-02-02 00:00:00','biasa','biasa',2,34,NULL,NULL,'selesai',1,0,NULL),
+(4,'masuk','415.4/151/V.18/I/2022','2022-01-26 00:00:00','Inventarisasi Penyelenggaraan Kerjasama Daerah','2022-02-02 15:21:37','rahasia','biasa',NULL,NULL,NULL,NULL,'diterima',1,1,NULL),
+(5,'masuk','045/151/V.18/I/2021','2022-02-04 00:00:00','Penyusunan Analisis Resiko','2022-02-05 15:22:35','biasa','biasa',1,32,NULL,NULL,'diproses',1,0,NULL),
+(6,'masuk','041/186/V.18/IV/2022','2022-02-15 00:00:00','Undangan Menghadiri Pembukaan BimTek Pengelolaan HomeStay','2022-04-19 15:23:43','biasa','biasa',2,33,NULL,NULL,'selesai',1,1,'2022-04-25 05:55:48'),
+(7,'internal','003/DPRD/04/2022','2022-04-15 00:00:00','Inventarisasi Penyelenggaraan Kerjasama Daerah','2022-04-19 00:00:00','biasa','biasa',33,34,NULL,NULL,'selesai',1,0,NULL),
+(8,'internal','rgert345345','2022-04-05 00:00:00','dgfdfgd','2022-04-11 00:00:00','biasa','biasa',33,32,NULL,'7ace378b77a447e52527255189bcc1ce.png','selesai',1,0,NULL),
+(11,'keluar','bchfjghj','2022-04-21 00:00:00','bcbcvb','2022-04-25 00:00:00','biasa','segera',1,33,NULL,'faaaa43bc3903d6ad8225198b02da7d9.png','diterima',1,1,'2022-04-25 06:15:29'),
+(16,'keluar','B-DL.01.01/184/2023','2022-04-19 00:00:00','Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.','2022-04-14 00:00:00','rahasia','biasa',40,3,NULL,'f610e22e15ea913550e3398110f80de8.png','diterima',2,0,NULL),
+(18,'masuk','B-DL.01.01/184/2022','2022-04-06 00:00:00','fgdgdgf','2022-04-07 00:00:00','biasa','biasa',2,33,NULL,'0049e7c16d89a15b0b41c18996c755fc.png','diproses',2,0,NULL),
+(19,'masuk','B-DL.01.01/184/2025','2022-04-06 00:00:00','Curabitur aliquet quam id dui posuere blandit.','2022-04-13 00:00:00','rahasia','biasa',2,37,'fgdg1234','71d37f433a89503d1b4c02d578444a85.png','diterima',2,0,NULL),
+(20,'masuk','800/8296/UT.01/2022','2022-04-01 00:00:00','Pendaftaran email official ASN di lingkungan Pemprov Lampung','2022-04-27 00:00:00','biasa','biasa',2,37,'6078','0b56c1bba4dece617ec1b01206f6dadf.pdf','diterima',1,0,NULL),
+(21,'masuk','B-DL.01.01/184/2022','2022-03-31 00:00:00','Proin eget tortor risus.','2022-04-06 00:00:00','biasa','biasa',6,34,'fgdg1234','56a8dd12b0b26b1038b5450e3c35377b.png','diterima',2,0,NULL);
 
 /*Table structure for table `tipe_disposisi` */
 
@@ -212,6 +250,7 @@ insert  into `users`(`id`,`username`,`password`,`nama_lengkap`,`nip`,`id_jabatan
 (1,'ade','$2y$10$xPgY2dw2kCImv2JykoYTSug4/ImjmzZ4AjDD1713TO.1BEtZgFNVK','Ade Rahman, S.Kom.','123456789',33,'admin',0,'2022-04-26 15:41:28',0,NULL),
 (2,'andri','$2y$10$rK1FmRohlTsq40LrLdtEauLrsNC8BcM6UrJNLZykoC6M8kDJ0PDpC','Andri Ahmad Mukarom, S.Kom., M.Ti.','987654321123',32,'admin',0,'2022-04-26 15:41:30',0,NULL),
 (3,'cinda','$2y$10$oriATDnGkazaYdmEchuDCOZ1R4h3gTBh.0dnhZRggqPkDpt2AZtZy','Faeyza Armada','dfdfdf',32,'admin',0,'2022-04-26 15:41:32',1,'2022-04-23 20:28:17'),
+(4,'123','$2y$10$7PVjt7izM0dzh8/rkEDizOzZYb8ImnydNcbiuDSAmctUtauZ9rnBG','sdfsdfs','dsfdsf',39,'admin',0,'2022-04-26 15:41:34',1,'2022-04-25 06:13:34'),
 (5,'admin','$2y$10$8Ssr803BAa9o9mWO4JIHw.hBsXFEUGCX67fhsrBGQ9D7kkzr/Pskq','Admin','123456789',32,'admin',0,NULL,0,NULL);
 
 /*Table structure for table `users_back` */
